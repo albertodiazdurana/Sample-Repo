c33c9e067424cecfd7cd3d21b5d227798a68b41c
Alberto Diaz <adiazdurana@gmail.com>
2019-10-10 01:32:20 +0200

    Removed some code

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-import math
-import os
-import sys
-
 import requests
 
 
-def greet(who_to_greet):
-    test_a = "Test"
-    n = 3
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-print(sys.executable)
 r = requests.get("https://hedera.online")
 print(r.status_code)
-
-# print (sys.version)
+print (r.ok)
